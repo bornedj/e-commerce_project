5b6e49a1167415ffeaa22355dca0084fcf0c26a7
bornedj <bornedj@miamioh.edu>
2022-01-28 11:29:16 -0500

    fixed import issues and removed keys from github

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -1,16 +1,16 @@
 module.exports = {
-    type: 'postgres',
-    database: process.env.DBNAME,
-    username: process.env.DBUSERNAME,
-    password: process.env.DBPASSWORD,
-    host: process.env.DBHOST,
-    logging: ["error", "migration", "query"],
-    port: 5432,
-    synchronize: process.env.DEV,
-    entities: ['./src/entities/*.ts'],
-    migrations: ["./src/migrations/*.ts"],
-    migrationsRun: true,
-    cli: {
-        "migrationsDir": "./src/migrations"
-    }
-}
+  type: "postgres",
+  database: process.env.DBNAME,
+  username: process.env.DBUSERNAME,
+  password: process.env.DBPASSWORD,
+  host: process.env.DBHOST,
+  logging: ["error", "migration", "query"],
+  port: 5432,
+  synchronize: process.env.DEV,
+  entities: ["./src/entities/*.ts"],
+  migrations: ["./src/migrations/*.ts"],
+  migrationsRun: true,
+  cli: {
+    migrationsDir: "./src/migrations",
+  },
+};
